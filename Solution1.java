7b82d5f0aae18efe98c4aabfd476f3e1248c71dc
amardkumar <102793539+amardkumar@users.noreply.github.com>
2025-05-21 21:09:23 -0400

    COin Change Implementation

---
+++ Solution1.java
@@ -0,0 +1,19 @@
+//Time Complexity:  O(S*n) 
+//where S is the amount and n is the number of coins
+//Space Complexity : 0(S) 
+
+
+class Solution1 {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount+1];
+        Arrays.fill(dp, amount+1);
+        dp[0] = 0;
+
+        for(int coin : coins){
+            for(int i = coin; i<=amount; i++){
+                dp[i] = Math.min(dp[i], dp[i-coin]+1);
+            }
+        }
+        return dp[amount] <= amount ? dp[amount] : -1;
+    }
+}
